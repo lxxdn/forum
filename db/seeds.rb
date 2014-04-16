1fd78871a8a7e319853fa65be1440ba3eed88948
Xiao LU <julien.luxiao@gmail.com>
2014-04-16 23:32:12 +0200

    modify seeds by chinese

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts 'SETTING UP CATALOG'
-tech = Catalog.create! name: 'Technology'
+tech = Catalog.create! name: '技术'
 apple = Catalog.create! name: 'Apple'
-idea = Catalog.create! name: 'Idea'
+idea = Catalog.create! name: '想法'
 
 puts 'SETTING UP topics'
 tech.topics.create! name: 'Ruby'
@@ -20,15 +20,18 @@ apple.topics.create! name: 'OSX'
 apple.topics.create! name: 'MBP'
 apple.topics.create! name: 'iPhone'
 
-idea.topics.create! name: 'Design'
-idea.topics.create! name: 'Share'
-idea.topics.create! name: 'Jobs'
+idea.topics.create! name: '设计'
+idea.topics.create! name: '分享'
+idea.topics.create! name: '求职'
 
 puts 'SETTING UP DEFAUT USER'
 User.create! :name => 'lxxdn', :email => 'julien.luxiao@gmail.com', :password => '12345678', :password_confirmation => '12345678'
 
 puts 'SETTNG UP TIPS'
-Tip.create! content: "Ruby is a dynamic language"
-Tip.create! content: "Matz said: Use Ruby 2.0"
-Tip.create! content: "The forum is developped by ROR"
-Tip.create! content: "Ruby 2.1 is comming soon"
+Tip.create! content: "棉被吹走了"
+Tip.create! content: "Matz 说: 用 Ruby 2.0"
+Tip.create! content: "我和我的小伙伴们都震惊了"
+Tip.create! content: "你吓到我了"
+Tip.create! content: "完工啦"
+Tip.create! content: "毛主席说：Go是一个好同志"
+Tip.create! content: "不要在意这些细节"
