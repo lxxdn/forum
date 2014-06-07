e49c7820a8bc0b64ee8c6a422494c12381bbe17e
Xiao LU <julien.luxiao@gmail.com>
2014-06-07 15:12:53 +0200

    modify deploy ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/lxdn/tassadar'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/mongoid.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -36,7 +36,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 # deploy.rb or stage file (staging.rb, production.rb or else)
 set :rvm_type, :user                     # Defaults to: :auto
-set :rvm_ruby_version, '2.1.2-p95'      # Defaults to: 'default'
+set :rvm_ruby_version, '2.1.1'      # Defaults to: 'default'
 
 namespace :deploy do
 
