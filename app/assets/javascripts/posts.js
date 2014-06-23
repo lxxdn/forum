a5e7705e72857af34bea5f5d9d13a9dc52d12ce9
Xiao LU <julien.luxiao@gmail.com>
2014-06-23 16:29:37 +0200

    fix js bug

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,15 +1,35 @@
 (function($) {
-  $(document).ready(function(){
-    $('.btn-write').addClass("active");
+  $('.btn-write').addClass("active");
 
-    $('.btn-write').click(function(){
+  $('.btn-write').click(function(){
+    $(this).addClass("active");
+      $('.btn-preview').removeClass("active");
+  });
+
+  $('.btn-preview').click(function(){
       $(this).addClass("active");
-        $('.btn-preview').removeClass("active");
+      $('.btn-write').removeClass("active");
     });
 
-    $('.btn-preview').click(function(){
-        $(this).addClass("active");
-        $('.btn-write').removeClass("active");
-      });
+  $("#content-markdown").hide();
+  $(".btn-write").click(function(){
+    $("#post_content").show();
+    $("#content-markdown").hide();
+  });
+  $(".btn-preview").click(function(){
+    $("#post_content").hide();
+    $("#content-markdown").empty();
+    $("#content-markdown").append("loading...");
+    $("#content-markdown").show();
+    $.ajax({
+      url: "/posts/transform_markdown.json",
+      dataType: "json",
+      data: {raw_markdown: $("#post_content").val()},
+      type: 'PUT',
+    })
+    .done(function( data ) {
+      $("#content-markdown").empty();
+      $("#content-markdown").append(data['data']);
     });
+  });
 })(jQuery);
--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,16 +1,13 @@
 (function($) {
-  $(document).ready(function(){
-    function readURL() {
-      var input = this;
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-              $("#user-image").attr('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-      }
-    };
-
-    $("#profile_image").change(readURL);
-  });
+  function readURL() {
+    var input = this;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $("#user-image").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+  };
+  $("#profile_image").change(readURL);
 })(jQuery);
